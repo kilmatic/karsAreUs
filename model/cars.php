2ee4515cddda983a5905a5b777b9ce7eb5ddbaab
git@github.com:kilmatic/karsAreUs.git <kilakanya@gmail.com>
2019-02-18 13:19:46 +0200

    CRUD functions

--- model/cars.php
+++ model/cars.php
@@ -16,5 +16,81 @@
         }
     }
 
+    function add_cars($ref, $make, $model, $man_year, $mileage, $_location, $price, $colour, $transmission)
+    {
+        global $db;
+        $query = 'INSERT INTO cars
+                    (ref, make, model, man_year, mileage, _location, price, colour, transmission)
+                  VALUES
+                    (:ref, :make, :model, :man_year, :mileage, :_location, :price, :colour, :transmission)';
+        try
+        {
+            $statement = $db->prepare($query);
+            $statement->bindValue(':ref', $ref);
+            $statement->bindValue(':make', $make);
+            $statement->bindValue(':model', $model);
+            $statement->bindValue(':man_year', $man_year);
+            $statement->bindValue(':mileage', $mileage);
+            $statement->bindValue(':_location', $_location);
+            $statement->bindValue(':price', $price);
+            $statement->bindValue(':colour', $colour);
+            $statement->bindValue(':transmission', $transmission);
+            $statement->execute();
+            $statement->closeCursor();
+            $car_ref = $db->lastInsertedId();
+            return $car_ref;
+        }
+        catch(PDOexception $e)
+        {
+            $error_message = $e->getMessage();
+            display_db_error($error_message);
+        }
+    }
 
+    function update_cars($ref, $make, $model, $man_year, $mileage, $_location, $price, $colour, $transmission)
+    {
+        global $db;
+        $query = 'UPDATE cars SET ref = :ref, make = :make, model = :model, man_year = :man_year, mileage = :mileage, _location = :_location, price = :price, colour = :colour, transmission = :transmission 
+                  WHERE ref = :ref';
+        try
+        {
+            $statement = $db->prepare($query);
+            $statement->bindValue(':ref', $ref);
+            $statement->bindValue(':make', $make);
+            $statement->bindValue(':model', $model);
+            $statement->bindValue(':man_year', $man_year);
+            $statement->bindValue(':mileage', $mileage);
+            $statement->bindValue(':_location', $_location);
+            $statement->bindValue(':price', $price);
+            $statement->bindValue(':colour', $colour);
+            $statement->bindValue(':transmission', $transmission);
+            $statement->execute();
+            $statement->closeCursor();
+            $car_ref = $db->lastInsertedId();
+            return $car_ref;
+        }   
+        catch(PDOexception $e)
+        {
+            $error_message = $e->getMessage();
+            display_db_error($error_message);
+        }        
+    }
+
+    function delete_cars($ref)
+    {
+        global $db;
+        try
+        {
+            $statement = $db->prepare('DELETE FROM cars WHERE ref = :ref');
+            $statement->bindValue(':ref', $ref);
+            $row_count = $statement->execute();
+            $statement->clodeCursor();
+            return $row_count;
+        }
+        catch(PDOexception $e)
+        {
+            $error_message = $e->getMessage();
+            display_db_error($error_message);
+        }
+    }
 ?>
