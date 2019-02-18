4ceb6126a57001f270fca0863022e9985b2e92b0
git@github.com:kilmatic/karsAreUs.git <kilakanya@gmail.com>
2019-02-18 16:37:09 +0200

    CRUD functions final...

--- model/cars.php
+++ model/cars.php
@@ -4,7 +4,25 @@
         global $db;
         try
         {
-            $statement = $db->prepare('SELECT * FROM cars ORDER BY make');
+            $statement = $db->prepare('SELECT * FROM cars');
+            $statement->execute();
+            $result = $statement->fetchAll();
+            $statement->closeCursor();
+            return $result;
+        }
+        catch(PDOexception $e)
+        {
+            display_db_error($e->getMessage());
+        }
+    }
+
+    function get_cars_by_ref($ref)
+    {
+        global $db;
+        try
+        {
+            $statement = $db->prepare('SELECT * FROM cars WHERE ref = :ref ORDER BY ref');
+            $statement->bindValue(':ref', $ref);
             $statement->execute();
             $result = $statement->fetchAll();
             $statement->closeCursor();
