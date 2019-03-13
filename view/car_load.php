faac0c7fc2e7130b615ac11444e5bba84f5dcf8c
git@github.com:kilmatic/karsAreUs.git <kilakanya@gmail.com>
2019-03-13 14:53:34 +0200

    car card with footer

--- view/car_load.php
+++ view/car_load.php
@@ -22,18 +22,20 @@ require_once 'model/cars_db.php';
     <?php foreach($get_cars as $car) : ?>
         <div class="col-md-4">
             <div class="card mb-4 shadow-sm">
-            <h5><?php echo $car['make'].' '.$car['model']; ?></h5>
-            <img src="images\TT.jpeg" alt="TT" class="card-img-top" width="80%" height="225">
-            <div class="card-body">
-                <p class="card-text"><?php echo $car['car_description']; ?></p>
-                <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Enquire</button>
+                <img src="images\TT.jpeg" alt="TT" class="card-img-top" width="80%" height="225">
+                <div class="card-body">
+                    <h5><?php echo $car['make'].' '.$car['model']; ?></h5>
+                    <p class="card-text"><?php echo $car['car_description']; ?></p>                    
                 </div>
-                <small class="text-muted"><?php echo 'R'.$car['price']; ?></small>
+                <div class="card-footer">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Enquire</button>
+                        </div>
+                        <small class="text-muted"><?php echo 'R'.$car['price']; ?></small>                    
+                    </div>                  
                 </div>
-            </div>
             </div>
         </div>
     <?php endforeach; ?>
