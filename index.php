439f2b8fbb9b81178e939338639afde9f774e0e1
git@github.com:kilmatic/karsAreUs.git <kilakanya@gmail.com>
2020-01-16 22:33:23 +0200

    finished styling filter buttons...

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <title>KarsAreUs</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="node_modules/ion-rangeslider/css/ion.rangeSlider.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/css/ion.rangeSlider.min.css"/>
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/css/ion.rangeSlider.min.css"/> -->
 </head>
 <body>
     <?php require_once 'view/nav.php'; ?>    
@@ -42,13 +42,13 @@
         </div>             -->
     </div>
 <?php require_once 'view/footer.php'; ?>
-<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+<!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
 <script src="node_modules\jquery\dist\jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> -->
-<script src="node_modules\ion-rangeslider\js\ion.rangeSlider.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/js/ion.rangeSlider.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> -->
 <script src="node_modules\popper.js\dist\popper.min.js"></script>
+<script src="node_modules\ion-rangeslider\js\ion.rangeSlider.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/ion-rangeslider/2.3.1/js/ion.rangeSlider.min.js"></script> -->
 <script src="js\nav.js"></script>
 <script src="js\carousel.js"></script>
 <script src="js\script.js"></script>
--- view/filter.php
+++ view/filter.php
@@ -1,23 +1,10 @@
 <?php require_once 'controller\filterController.php'; ?>
 <div class="filter-card">
-    <!-- <label for="form-control">Make</label>         
-    <select class="form-control">
-        <?php foreach($get_cars as $car) : ?>                   
-            <option><?php echo $car['make']; ?></option>
-        <?php endforeach; ?>
-    </select>  
-    <label for="form-control">Model</label>         
-    <select class="form-control">
-        <?php foreach($get_cars as $car) : ?>
-            <option><?php echo $car['model']; ?></option>
-        <?php endforeach; ?>
-    </select> -->
-
-    <!-- <div class="filter-make-model">
-        <ul class="">
-            <li id="submenu">
+    <div class="filter-make-model">
+        <ul>
+            <li>
                 <a href="#">Make / Model</a>
-                <ul>
+                <ul class="filter-dropdown">
                     <?php foreach($get_cars as $car) : ?>
                         <li class="nav-submenu">
                             <input type="checkbox" name="make" id="car-make"><?php echo $car['make']; ?>
@@ -26,31 +13,46 @@
                 </ul>
             </li>
         </ul>
-    </div> -->
+    </div>
 
-    <label for="form-control">Location</label>
-    <select class="form-control">
-        <?php foreach($get_cars as $car) : ?>
-            <option><?php echo $car['location']?></option>
-        <?php endforeach; ?>
-    </select>
-    <label for="form-control">Colour</label>
-    <select class="form-control">
-        <?php foreach($get_cars as $car) : ?>
-            <option><?php echo $car['colour']?></option>
-        <?php endforeach; ?>
-    </select><hr>
-    <!-- <span class="form-control">
-        <label>Price </label>
-        <input type="range" name="price" id="price" min="<?php ?>" max="<?php $get_max_price; ?>" step="1000" onchange="showPrice.value=value">
-        <output id="showPrice"></output>
-    </span> -->
+    <div class="filter-make-model">
+        <ul>        
+            <li>
+                <a href="#">Location</a>    
+                <ul class="filter-dropdown">
+                    <?php foreach($get_cars as $car) : ?>
+                        <li>
+                            <input type="checkbox" name="make" id="car-make"><?php echo $car['location']; ?>
+                        </li>
+                    <?php endforeach; ?>    
+                </ul>                    
+            </li>
+        </ul>
+    </div>
+
+    <div class="filter-make-model">
+        <ul>        
+            <li>
+                <a href="#">Colour</a>    
+                <ul class="filter-dropdown">
+                    <?php foreach($get_cars as $car) : ?>
+                        <li>
+                            <input type="checkbox" name="make" id="car-make"><?php echo $car['colour']; ?>
+                        </li>
+                    <?php endforeach; ?>    
+                </ul>                    
+            </li>
+        </ul>
+    </div>
+
     <input type="text" class="js-range-slider" name="my_range" value="" /><br>
+
     <span class="form-control">
         <label>Mileage </label>            
         <input type="range" name="price" id="mileage" min="<?php ?>" max="<?php ?>" step="100" onchange="showMiles.value=value">
         <output id="showMiles"></output>
     </span>
+
     <span class="form-control">
         <label>Year </label>            
         <input type="range" name="price" id="mileage" min="<?php ?>" max="<?php ?>" step="100" onchange="showYear.value=value">
