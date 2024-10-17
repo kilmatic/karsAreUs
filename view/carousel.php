5bd886c2c68c77f31561e4c0c16d67786d3d6fb8
kilmatic <kilakanya@gmail.com>
2024-10-17 10:15:30 +0200

    swiffy slider for carousel

--- view/carousel.php
+++ view/carousel.php
@@ -1,16 +1,16 @@
-<div class="carousel">
-    <div class="slider-container">
-        <ul class="slider">
-            <li class="slider-image currSlide" id="slide1"><img src="images\header\lexus-nx-f-sport-silver-luxurious-car-website-header.jpg" alt="slide1"></li>
-            <li class="slider-image" id="slide2"><img src="images\header\bmw-4-gran-coupe-car-and-desert-landscape-web-header.jpg" alt="slide2"></li>
-            <li class="slider-image" id="slide3"><img src="images\header\toyota-gt86-dashboard-car-website-header.jpg" alt="slide3"></li>
-        </ul>
+<div class="swiffy-slider slider-nav-autoplay" data-slider-nav-autoplay-interval="3500">
+    <ul class="slider-container">
+        <li><img src="images\header\lexus-nx-f-sport-silver-luxurious-car-website-header.jpg" style="min-width: 100%;height: auto;"></li>
+        <li><img src="images\header\bmw-4-gran-coupe-car-and-desert-landscape-web-header.jpg" style="min-width: 100%;height: auto;"></li>
+        <li><img src="images\header\toyota-gt86-dashboard-car-website-header.jpg" style="min-width: 100%;height: auto;"></li>
+    </ul>
+
+    <button type="button" class="slider-nav"></button>
+    <button type="button" class="slider-nav slider-nav-next"></button>
+
+    <div class="slider-indicators">
+        <button class="active"></button>
+        <button></button>
+        <button></button>
     </div>
-    <div class="points">
-        <button class="pointDot currSlide"></button>
-        <button class="pointDot"></button>
-        <button class="pointDot"></button>
-    </div>
-    <div class="arrow" id="next" data-dir="next">&raquo;</div>
-    <div class="arrow" id="prev" data-dir="prev">&laquo;</div>
 </div>
